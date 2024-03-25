08755a15009f250184fa84be47f2be2df0da0d64
maulishq <ishaquac14@gmail.com>
2024-03-25 22:15:10 +0700

    update kedua

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -21,16 +21,30 @@ class MessageController extends Controller
         ]);
 
         $token = "v2n49drKeWNoRDN4jgqcdsR8a6bcochcmk6YphL6vLcCpRZdV1";
-        $message = sprintf("-------------------------%c%s%c------------------------- ", 10, $request->input('message'), 10);
+        $nama_pengirim = $request->input('name');
+        $isi = $request->input('message');
+        $nomor = $request->input('phone_number');
 
-        $response = Http::post('https://app.ruangwa.id/api/send_message', [
-            'token' => $token,
-            'number' => $request->input('phone_number'),
-            'message' => $message,
-        ]);
+        $message = sprintf("----------SEND MESSAGE----------\n%s\n-----------------------------------\nFrom : %s", $isi, $nama_pengirim);
+
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://app.ruangwa.id/api/send_message',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => 'token=' . $token . '&number=' . $nomor . '&message=' . $message,
+        ));
+
+        $response = curl_exec($curl);
+        curl_close($curl);
 
         // Check if request was successful
-        if ($response->successful()) {
+        if ($response) {
             return redirect()->back()->with('success', 'Message sent successfully!');
         } else {
             return redirect()->back()->with('error', 'Failed to send message. Please try again.');
